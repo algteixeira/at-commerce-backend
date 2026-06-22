576564ca0bf2855cb5e7055cc8a73361db02aeb2
Andre Teixeira <andre.teixeira@compasso.com.br>
2026-06-22 19:51:55 -0300

    [test] Adding coverage and new tests for the user routes

--- src/tests/userController.test.js
+++ src/tests/userController.test.js
@@ -53,6 +53,23 @@ describe('UserController', () => {
             );
         });
 
+        test('Must throw an error if username already exists in the database', async () => {
+            mockReq = {
+                body: { username: 'luiz', password: '123' }
+            };
+
+            bcrypt.hash.mockResolvedValue('encrypte_pass');
+
+            prisma.user.findUnique.mockResolvedValue({ id: 1, username: 'luiz' });
+
+            await userController.create(mockReq, mockRes);
+
+            expect(mockRes.status).toHaveBeenCalledWith(400);
+            expect(mockRes.json).toHaveBeenCalledWith(
+                expect.objectContaining({ message: 'This username is already registered. Try another one.' })
+            );
+        });
+
         test('Must return error if missing user/pass', async () => {
             mockReq = { body: { username: '', password: '123' } };
 
@@ -64,60 +81,87 @@ describe('UserController', () => {
                 message: "Missing username/password!"
             });
         });
+
+        test('Must return an error while trying to access the database', async () => {
+            mockReq = { body: { username: 'luiz', password: '123' } };
+
+            prisma.user.findUnique.mockRejectedValue(new Error());
+            await userController.create(mockReq, mockRes);
+
+            expect(mockRes.status).toHaveBeenCalledWith(500);
+            expect(mockRes.json).toHaveBeenCalledWith({
+                error: 'DATABASE ERROR',
+                message: 'We had a problem accessing the database. Try again later.'
+            });
+        });
     });
 
     describe('Method: login', () => {
-    test('Must give login permission if existing data was provided!', async () => {
-        mockReq = { body: { username: 'luiz', password: '123' } };
-        bcrypt.compare.mockResolvedValue(true);
-        prisma.user.findUnique.mockResolvedValue({ username: 'luiz'});
+        test('Must give login permission if existing data was provided!', async () => {
+            mockReq = { body: { username: 'luiz', password: '123' } };
+            bcrypt.compare.mockResolvedValue(true);
+            prisma.user.findUnique.mockResolvedValue({ username: 'luiz' });
 
-        await userController.login(mockReq, mockRes);
-        
-        expect(mockRes.status).toHaveBeenCalledWith(200);
-        expect(mockRes.json).toHaveBeenCalledWith({ message: "Access granted!" });
-    });
+            await userController.login(mockReq, mockRes);
 
-    test('Must block access when no user or password was provided', async () => {
-        mockReq = { body: { username: '', password: '123' } };
+            expect(mockRes.status).toHaveBeenCalledWith(200);
+            expect(mockRes.json).toHaveBeenCalledWith({ message: "Access granted!" });
+        });
 
-        prisma.user.findUnique.mockResolvedValue({ username: '' });
+        test('Must block access when no user or password was provided', async () => {
+            mockReq = { body: { username: '', password: '123' } };
 
-        await userController.login(mockReq, mockRes);
-        
-        expect(mockRes.status).toHaveBeenCalledWith(400);
-        expect(mockRes.json).toHaveBeenCalledWith({
-            error: "AUTH",
-            message: "Missing username/password!"
+            prisma.user.findUnique.mockResolvedValue({ username: '' });
+
+            await userController.login(mockReq, mockRes);
+
+            expect(mockRes.status).toHaveBeenCalledWith(400);
+            expect(mockRes.json).toHaveBeenCalledWith({
+                error: "AUTH",
+                message: "Missing username/password!"
+            });
+        });
+
+        test('Must block access when wrong username was provided', async () => {
+            prisma.user.findUnique.mockResolvedValue(null);
+
+            mockReq = { body: { username: 'xisculous', password: 'danone' } };
+
+            await userController.login(mockReq, mockRes);
+
+            expect(mockRes.status).toHaveBeenCalledWith(400);
+            expect(mockRes.json).toHaveBeenCalledWith({
+                error: "AUTH",
+                message: "Wrong credentials!"
+            });
+        });
+
+        test('Must block access when wrong password was provided', async () => {
+            prisma.user.findUnique.mockResolvedValue({ id: 3, username: 'xisculous', password: 'hashed_pass' });
+            bcrypt.compare.mockResolvedValue(false);
+            mockReq = { body: { username: 'xisculous', password: 'danone' } };
+
+            await userController.login(mockReq, mockRes);
+
+            expect(mockRes.status).toHaveBeenCalledWith(400);
+            expect(mockRes.json).toHaveBeenCalledWith({
+                error: "AUTH",
+                message: "Wrong credentials!"
+            });
+        });
+
+        test('Must throw errors when losing connection to the database', async () => {
+            prisma.user.findUnique.mockRejectedValue(new Error());
+            bcrypt.compare.mockResolvedValue(false);
+            mockReq = { body: { username: 'xisculous', password: 'danone' } };
+
+            await userController.login(mockReq, mockRes);
+
+            expect(mockRes.status).toHaveBeenCalledWith(500);
+            expect(mockRes.json).toHaveBeenCalledWith({
+                error: 'DATABASE ERROR',
+                message: 'We had problems while accessing the database. Try again later.'
+            });
         });
     });
-
-    test('Must block access when wrong username was provided', async () => {
-        prisma.user.findUnique.mockResolvedValue(null);
-        
-        mockReq = { body: { username: 'xisculous', password: 'danone' } };
-        
-        await userController.login(mockReq, mockRes);
-        
-        expect(mockRes.status).toHaveBeenCalledWith(400);
-        expect(mockRes.json).toHaveBeenCalledWith({
-            error: "AUTH",
-            message: "Wrong credentials!"
-        });
-    });
-
-    test('Must block access when wrong password was provided', async () => {
-        prisma.user.findUnique.mockResolvedValue({id:3, username: 'xisculous', password: 'hashed_pass'});
-        bcrypt.compare.mockResolvedValue(false);
-        mockReq = { body: { username: 'xisculous', password: 'danone' } };
-        
-        await userController.login(mockReq, mockRes);
-        
-        expect(mockRes.status).toHaveBeenCalledWith(400);
-        expect(mockRes.json).toHaveBeenCalledWith({
-            error: "AUTH",
-            message: "Wrong credentials!"
-        });
-    });
-});
 });
